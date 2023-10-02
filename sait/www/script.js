a2cf0e768923de887cd3444fc60bf8655fc62e18
Troub <matveyv2004@gmail.com>
2023-10-02 20:12:38 +0300

    Dark and light themes in other files

--- sait/www/script.js
+++ sait/www/script.js
@@ -5,3 +5,22 @@ themeButton.onclick = function() {
   page.classList.toggle('light-theme');
   page.classList.toggle('dark-theme');
 };
+/*
+function applyTheme() {
+      const isDark = (localStorage.getItem('is-dark-theme') ?? 'true') === 'true';
+      if (isDark) {
+          page.classList.remove('light-theme');
+          page.classList.add('dark-theme');
+      } else {
+          page.classList.remove('dark-theme');
+          page.classList.add('light-theme');
+      }
+  }
+function themeChange() {
+  localStorage.setItem('is-dark-theme', !((localStorage.getItem('is-dark-theme') ?? 'true') === 'true'));
+  applyTheme();
+}
+window.addEventListener('DOMContentLoaded', function () {
+       applyTheme();
+});
+*/
