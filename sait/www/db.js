37635333227b4ea7df66e4194b0422dc27e0797c
Алексей Щелкунов <7183704a@gmail.com>
2023-12-04 18:39:43 +0300

    db

--- sait/www/db.js
+++ sait/www/db.js
@@ -1,9 +1,18 @@
-const express = require('express');
 const sqlite3 = require('sqlite3').verbose();
+const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const {send} = require("express/lib/response");
+
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  }),
+);
 app.use(express.json());
 
-let db = new sqlite3.Database('./mydb.sqlite', (err) => {
+let db = new sqlite3.Database('./db.sqlite', (err) => {
   if (err) {
     return console.error(err.message);
   }
@@ -11,25 +20,26 @@ let db = new sqlite3.Database('./mydb.sqlite', (err) => {
 });
 
 db.serialize(() => {
-  db.run('CREATE TABLE IF NOT EXISTS user(username TEXT, password TEXT, email TEXT)');
+  db.run('CREATE TABLE IF NOT EXISTS users(username TEXT, password TEXT, email TEXT)');
 });
 
-app.post('/register', (req, res) => {
-  let username = req.body.username;
-  let password = req.body.password;
-  let email = req.body.email;
-
-  let stmt = db.prepare('INSERT INTO user(username , password , email ) VALUES (?, ? ,?)');
-  stmt.run(username,password,email);
-  stmt.finalize();
-
-  res.send('User registered successfully');
+app.post('/register', (req) => {
+    const stmt = db.prepare("INSERT INTO users ( username, password,email) VALUES(?, ?,?)");
+    stmt.run(req.body.username, req.body.password, req.body.email, (err) => {
+        if (err) {
+            console.error(err.message);
+            res.status(500).send('Error registering user');
+        } else {
+            res.send('User registered successfully');
+        }
+        stmt.finalize();
+    });
 });
-
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+app.listen(5000, () => {
+  console.log('Server is running on port 5000');
+  console.log(`App listening at http://localhost:${5000}`);
 });
 
 process.on('exit', () => {
-  db.close(); 
+  db.close();
 });
